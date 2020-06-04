8c8277d5a7697e413074e5c8e932553755153ad1
AymDev <aymericmayeux@gmail.com>
2020-06-04 23:53:00 +0200

    ADD temporary old keys columns management
    Fregata creates temporary columns before running migrations and
    removes them afterwards.
    These columns contain old primary keys to help set foreign keys in
    target database.

--- src/Fregata.php
+++ src/Fregata.php
@@ -5,8 +5,12 @@ namespace Fregata;
 use DI\Container;
 use DI\DependencyException;
 use DI\NotFoundException;
+use Doctrine\DBAL\Schema\Column;
+use Doctrine\DBAL\Schema\Index;
+use Doctrine\DBAL\Schema\TableDiff;
 use Fregata\Migrator\MigratorException;
 use Fregata\Migrator\MigratorInterface;
+use Fregata\Migrator\PreservedKeyMigratorInterface;
 use Psr\Container\ContainerInterface;
 
 class Fregata
@@ -24,6 +28,11 @@ class Fregata
      * @var MigratorInterface[]
      */
     private array $migrators = [];
+
+    /**
+     * @var string[] old primary keys column & index names in targets per migrator
+     */
+    private array $preservedKeys = [];
 
     /**
      * Fregata constructor
@@ -57,6 +66,8 @@ class Fregata
 
     /**
      * Get the registered migrators
+     *
+     * @throws MigratorException
      */
     public function run(): \Generator
     {
@@ -64,8 +75,102 @@ class Fregata
             throw new \LogicException('No migrators registered.');
         }
 
+        // Create temporary columns & indexes for foreign keys preservation
+        $this->createPreservedKeys();
+
+        // Run migration
         foreach ($this->migrators as $migrator) {
             yield $migrator;
+        }
+
+        // Delete temporary columns & indexes
+        $this->deletePreservedKeys();
+    }
+
+    /**
+     * Create temporary columns & indexes in target tables for foreign keys preservation
+     */
+    private function createPreservedKeys(): void
+    {
+        foreach ($this->migrators as $migrator) {
+            if ($migrator instanceof PreservedKeyMigratorInterface) {
+                // Create and save column name for future deletion
+                $columnName = sprintf('fregata_pk_%s', $migrator->getSourceTable());
+                $this->preservedKeys[get_class($migrator)]['column'] = $columnName;
+
+                // Create index for temporary column
+                $indexName = sprintf('fregata_idx_%s', $migrator->getSourceTable());
+                $this->preservedKeys[get_class($migrator)]['index'] = $indexName;
+                $index = new Index($indexName, [$columnName]);
+
+                // Get original column
+                $originalColumn = $migrator
+                    ->getSourceConnection()
+                    ->getConnection()
+                    ->getSchemaManager()
+                    ->listTableDetails($migrator->getSourceTable())
+                    ->getColumn($migrator->getPrimaryKeyColumnName());
+
+                // Change column settings as it won't be a primary key anymore
+                $temporaryColumn = (new Column($columnName, $originalColumn->getType()))
+                    ->setLength($originalColumn->getLength())
+                    ->setPrecision($originalColumn->getPrecision())
+                    ->setScale($originalColumn->getScale())
+                    ->setFixed($originalColumn->getFixed())
+                    ->setUnsigned($originalColumn->getUnsigned())
+                    ->setAutoincrement(false)
+                    ->setNotnull(false)
+                    ->setDefault(null);
+
+                // Add column into target table
+                $migrator
+                    ->getTargetConnection()
+                    ->getConnection()
+                    ->getSchemaManager()
+                    ->alterTable(new TableDiff(
+                        $migrator->getTargetTable(),
+                        [$temporaryColumn],
+                        [],
+                        [],
+                        [$index]
+                    ));
+            }
+        }
+    }
+
+    /**
+     * Create temporary columns & indexes in target tables
+     */
+    private function deletePreservedKeys(): void
+    {
+        foreach ($this->preservedKeys as $migratorClass => $key) {
+            /** @var MigratorInterface & PreservedKeyMigratorInterface $migrator */
+            $migrator = $this->migrators[$migratorClass];
+
+            $table = $migrator
+                ->getTargetConnection()
+                ->getConnection()
+                ->getSchemaManager()
+                ->listTableDetails($migrator->getTargetTable());
+
+            $temporaryIndex = $table->getIndex($key['index']);
+            $temporaryColumn = $table->getColumn($key['column']);
+
+            // Delete column and index
+            $migrator
+                ->getTargetConnection()
+                ->getConnection()
+                ->getSchemaManager()
+                ->alterTable(new TableDiff(
+                    $migrator->getTargetTable(),
+                    [],
+                    [],
+                    [$temporaryColumn],
+                    [],
+                    [],
+                    [$temporaryIndex],
+                    $table
+                ));
         }
     }
 }
--- tests/FregataTest.php
+++ tests/FregataTest.php
@@ -4,9 +4,17 @@
 namespace Fregata\Tests;
 
 
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Schema\Column;
+use Doctrine\DBAL\Schema\Index;
+use Doctrine\DBAL\Schema\Table;
+use Doctrine\DBAL\Types\IntegerType;
+use Doctrine\DBAL\Types\StringType;
+use Fregata\Connection\AbstractConnection;
 use Fregata\Fregata;
 use Fregata\Migrator\MigratorException;
 use Fregata\Migrator\MigratorInterface;
+use Fregata\Migrator\PreservedKeyMigratorInterface;
 
 class FregataTest extends FregataTestCase
 {
@@ -63,5 +71,89 @@ class FregataTest extends FregataTestCase
 
         self::assertCount(1, $migrators);
         self::assertInstanceOf(MigratorInterface::class, $migrators[0]);
+    }
+
+    /**
+     * Run with a migrator with preserved keys, temporary columns must be deleted after migration
+     */
+    public function testPreservedKeysColumnsAreDeleted()
+    {
+        // Create table in both databases (no dataset needed)
+        $table = new Table(
+            'fregata_test',
+            [
+                new Column('id', new IntegerType(), ['autoincrement' => true]),
+                new Column('col_str', new StringType(), ['notNull' => false]),
+            ],
+            [new Index('pk', ['id'], false, true)]
+        );
+
+        $this->createTargetTable($this->getMySQLConnection(), $table);
+        $this->createTargetTable($this->getPgSQLConnection(), $table);
+
+        // Create migrator
+        $migrator = new class implements MigratorInterface, PreservedKeyMigratorInterface {
+            public function getSourceConnection(): AbstractConnection
+            {
+                return new class extends AbstractConnection {
+                    public string $url = 'mysql://root:root@127.0.0.1:3306/fregata_source';
+                };
+            }
+
+            public function getTargetConnection(): AbstractConnection
+            {
+                return new class extends AbstractConnection {
+                    public string $url = 'pgsql://postgres:postgres@127.0.0.1:5432/fregata_target';
+                };
+            }
+
+            public function getTotalRows(Connection $source): int
+            {
+                return 1;
+            }
+
+            public function migrate(Connection $source, Connection $target): \Generator
+            {
+                yield 1;
+            }
+
+            public function getPrimaryKeyColumnName(): string
+            {
+                return 'id';
+            }
+
+            public function getSourceTable(): string
+            {
+                return 'fregata_test';
+            }
+
+            public function getTargetTable(): string
+            {
+                return 'fregata_test';
+            }
+        };
+
+        $schemaManager = $this->getPgSQLConnection()
+            ->getConnection()
+            ->getSchemaManager();
+
+        // Before migrations
+        $columnsBefore = $schemaManager->listTableColumns('fregata_test');
+        $indexesBefore = $schemaManager->listTableIndexes('fregata_test');
+
+        // Run migration
+        $fregata = (new Fregata())
+            ->addMigrator(get_class($migrator));
+
+        foreach ($fregata->run() as $migration) {
+            /* just run it */
+        }
+
+        // After migrations
+        $columnsAfter = $schemaManager->listTableColumns('fregata_test');
+        $indexesAfter = $schemaManager->listTableIndexes('fregata_test');
+
+        self::assertSame(array_keys($columnsBefore), array_keys($columnsAfter));
+        self::assertSame(array_keys($indexesBefore), array_keys($indexesAfter));
     }
 }
