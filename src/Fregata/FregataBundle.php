f89af89732bd01013c5209a1bd92bf969ed7625d
Andreas Rau <andreas.rau@check24.de>
2021-10-20 15:56:52 +0200

    register commands

--- src/Fregata/FregataBundle.php
+++ src/Fregata/FregataBundle.php
@@ -4,8 +4,20 @@ declare(strict_types=1);
 
 namespace Fregata\Fregata;
 
+use Fregata\Console\MigrationExecuteCommand;
+use Fregata\Console\MigrationListCommand;
+use Fregata\Console\MigrationShowCommand;
+use Symfony\Component\Console\Application;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class FregataBundle extends Bundle
 {
+
+    public function registerCommands(Application $application)
+    {
+        parent::registerCommands($application);
+        $application->register(MigrationExecuteCommand::class);
+        $application->register(MigrationShowCommand::class);
+        $application->register(MigrationListCommand::class);
+    }
 }
