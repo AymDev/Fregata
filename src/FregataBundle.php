22b909c01fe8559bd35b59f52b1041b27d1bcee2
Andreas Rau <andreas.rau@check24.de>
2021-10-20 17:21:31 +0200

    add commands

--- src/FregataBundle.php
+++ src/FregataBundle.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Fregata;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class FregataBundle extends Bundle
@@ -14,5 +15,12 @@ final class FregataBundle extends Bundle
     public function __construct()
     {
         $this->name = self::NAME;
+    }
+
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new DependencyInjection\CommandsCompilerPass());
+        $container->addCompilerPass(new DependencyInjection\FregataCompilerPass());
     }
 }
